a3293e5ae12c3192eeaa661cd207d89bb6c028f8
Pravin Bansod <136956809+silentSK987@users.noreply.github.com>
2023-10-27 23:04:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function threeSum(arr, target) {
 // write your code here
+	let intialDiff = Infinity, ans = -1; 
+
+let n = arr.length;
+for (let i = 0; i <= n - 3; i++) {
+  for (let j = i + 1; j <= n - 2; j++) {
+    for (let k = j + 1; k <= n - 1; k++) {
+        let sum = arr[i] + arr[j] + arr[k] ;
+        let diff = Math.abs(sum - target);
+        if(intialDiff > diff) {
+            ans = sum;
+        }
+    }
+  }
+}
   
 }
 
