0a5dbba41114631c9243f8072cf76933d63e89d5
Pravin Bansod <136956809+silentSK987@users.noreply.github.com>
2023-10-27 23:17:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-function threeSum(arr, target) {
-// write your code here
-	let intialDiff = Infinity, ans = -1; 
-
-let n = arr.length;
-for (let i = 0; i <= n - 3; i++) {
-  for (let j = i + 1; j <= n - 2; j++) {
-    for (let k = j + 1; k <= n - 1; k++) {
-        let sum = arr[i] + arr[j] + arr[k] ;
-        let diff = Math.abs(sum - target);
-        if(intialDiff > diff) {
-            ans = sum;
-        }
+function threeSum(nums, target) {
+  nums.sort((a, b) => a - b); // sort the array in ascending order
+  let closestSum = Infinity; // initialize the closest sum to a large value
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+      if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+        closestSum = sum; // update the closest sum
+      }
+      if (sum < target) {
+        left++;
+      } else if (sum > target) {
+        right--;
+      } else {
+        return closestSum; // return the closest sum if we find an exact match
+      }
     }
   }
+  return closestSum; // return the closest sum at the end of the loop
 }
-  
-}
-
 module.exports = threeSum;
